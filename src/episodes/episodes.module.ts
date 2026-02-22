68d993392f23aa6f2847e5796a5895ae3e0b959e
MizeroR <r.mizero@alustudent.com>
2026-02-22 20:01:09 +0200

    feat: enhance EpisodesController and EpisodesModule with service integration and update main.ts to use fixed port

--- src/episodes/episodes.module.ts
+++ src/episodes/episodes.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Get, Module, Post } from '@nestjs/common';
 import { EpisodesController } from './episodes.controller';
 import { EpisodesService } from './episodes.service';
+import { ConfigModule } from 'src/config/config.module';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [EpisodesController],
   providers: [EpisodesService],
 })
