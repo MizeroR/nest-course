dfafb5ea2e16c99872d3a7a56a4aeb19c7076b92
MizeroR <r.mizero@alustudent.com>
2026-02-22 21:59:41 +0200

    feat: enhance findOne method in EpisodesController to handle not found cases and update tests accordingly

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -7,14 +7,17 @@ import { EpisodesService } from './episodes.service';
 describe('EpisodesController', () => {
   let controller: EpisodesController;
 
+  const mockFindOne = jest.fn();
+
   const mockEpisodesService = {
     findAll: () => [{ id: 'id' }],
     findFeaturedEpisodes: () => [{ id: 'id' }],
-    findOne: (id: string) => ({ id }),
+    findOne: mockFindOne,
     create: () => ({ id: 'id' }),
   };
 
   beforeEach(async () => {
+    jest.resetAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       imports: [ConfigModule],
       controllers: [EpisodesController],
@@ -29,10 +32,36 @@ describe('EpisodesController', () => {
   });
 
   describe('findOne', () => {
-    it('should return correct response', async () => {
+    describe('when episode is found', () => {
       const episodeId = 'id';
-      const result = await controller.findOne(episodeId);
-      expect(result).toEqual({ id: 'id' });
+      const mockResult = { id: episodeId, name: 'my episode' };
+
+      beforeEach(() => {
+        mockFindOne.mockResolvedValue(mockResult);
+      });
+
+      it('should call the service with the correct params', async () => {
+        await controller.findOne(episodeId);
+        expect(mockFindOne).toHaveBeenCalledWith(episodeId);
+      });
+
+      it('should return correct response', async () => {
+        const result = await controller.findOne(episodeId);
+        expect(result).toEqual({ id: 'id', name: 'my episode' });
+      });
+    });
+    describe('when episode is not found', () => {
+      const episodeId = 'id2';
+
+      beforeEach(() => {
+        mockFindOne.mockResolvedValue(null);
+      });
+
+      it('should throw an error', async () => {
+        await expect(controller.findOne(episodeId)).rejects.toThrow(
+          'Episode not found',
+        );
+      });
     });
   });
 });
